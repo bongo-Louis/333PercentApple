dd02a65a67165db513dad079d451d5f5d788820b
bongo-Louis <louis.h.bongo@gmail.com>
2026-08-11 14:46:18 +0800

    fixed and fully implimented the base pausemenu

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -1,15 +1,29 @@
 //Author:Jayden Ong
 //Description: Script to pause the game and switch between scenes
 
-using UnityEngine;
+using UnityEngine;  
+using StarterAssets;
 using UnityEngine.InputSystem; // 1. Include namespace
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenuUI;
     public static bool isPaused = false;
+    
+    // new update where we add cursor manager
+    private StarterAssetsInputs starterAssetsInputs;
 
     // 2. Unity automatically calls "OnPause" when the "Pause" Action is triggered
+
+    // find the starter assets input script in the scene
+    private void Awake()
+    {
+        starterAssetsInputs = FindObjectOfType<StarterAssetsInputs>();
+        if (starterAssetsInputs == null)
+        {
+            Debug.LogError("StarterAssetsInputs not found in the scene.");
+        }
+    }
     public void OnPause(InputValue value)
     {
         // Only toggle when the button is initially pressed down
@@ -31,6 +45,10 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+
+        // new update where we lock the camera from being moved when the game is paused
+        starterAssetsInputs.cursorInputForLook = false;
+        starterAssetsInputs.cursorLocked = false;
     }
 
     public void Resume()
@@ -38,5 +56,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+        starterAssetsInputs.cursorInputForLook = true;
+        starterAssetsInputs.cursorLocked = true;
     }
 }
